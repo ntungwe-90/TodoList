20df672c678ba02ed3b0b0907d47716be5381514
irene <ntongweirene@gmail.com>
2020-08-01 13:22:26 -0700

    TodoList

---
+++ src/TodoItems.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./TodoList.css";
+ 
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+     
+        this.createTasks = this.createTasks.bind(this);
+      }
+     
+      delete(key) {
+        this.props.delete(key);
+      }
+  createTasks(item) {
+    return <li key={item.key}>{item.text}</li>
+  }
+ 
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+ 
+    return (
+      <ul className="theList">
+          {listItems}
+      </ul>
+    );
+  }
+};
+ 
+export default TodoItems;
